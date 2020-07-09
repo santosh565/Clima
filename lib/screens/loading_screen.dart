ff6fb4a35b8e022d5e584b9070917178e09ea977
Santosh Bhattarai <hsotnas76@gmail.com>
2020-07-09 17:51:20 +0545

    showing a spinner

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,9 +1,8 @@
-
-
+import 'package:clima/screens/location_screen.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = '2ac6ab52b82b5c9c54b478d27ad52c61';
 
@@ -13,7 +12,6 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
   double latitude;
   double longitude;
   @override
@@ -28,18 +26,31 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
     latitude = location.latitude;
     longitude = location.longitude;
-    
-    NetworkHelper networkHelper = NetworkHelper('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
-    var weatherData  = await networkHelper.getData();
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
-    
+    var weatherData = await networkHelper.getData();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) {
+          return LocationScreen();
+        },
+      ),
+    );
   }
 
-   @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(),
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100,
+        ),
+      ),
     );
   }
 }
