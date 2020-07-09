3446acf3ecc0789a4fdc7dc7bae9d7be2115c3b7
Santosh Bhattarai <hsotnas76@gmail.com>
2020-07-09 14:32:50 +0545

    networking and adding http overrides

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:clima/screens/loading_screen.dart';
 
-void main() => runApp(MyApp());
+
+
+class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}
+void main(){
+    HttpOverrides.global = new MyHttpOverrides();
+    runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -20,8 +21,22 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  void getData() async {
+    
+      http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02');
+   if(response.statusCode ==200){
+     String data = response.body;
+     print(data);
+   }else{
+     print(response.statusCode);
+   }
+   
+   
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: Center(
         
